6554cad9739e2d55f6f4050349477411e107dc9a
aureaperezgutierrez <127677183+aureaperezgutierrez@users.noreply.github.com>
2023-03-24 11:37:26 +0100

    creado likes.js

---
+++ javascript/likes.js
@@ -0,0 +1,20 @@
+//INCREMENTAL  12
+contadorLikes = () => {
+    let cantLikes = 0;
+    let sumLikes = 0;
+    let fotosMenosDe10Likes = 0;
+    let numeroFotos = parseInt(prompt('Indique el número de fotos del usuario:'));
+
+    for (let i = 0; i < numeroFotos; i++) {
+          cantLikes = prompt(`Indique la cantidad de Likes de la Foto ${i+1}`);
+          sumLikes = sumLikes + parseInt(cantLikes);
+
+          if (parseInt(cantLikes) < 10) {
+                fotosMenosDe10Likes = fotosMenosDe10Likes + 1;
+          }
+    };
+    alert('La suma total de Likes es: ' + sumLikes);
+    alert('Fotos con menos de 10 Likes: ' + fotosMenosDe10Likes);
+};
+
+contadorLikes()
