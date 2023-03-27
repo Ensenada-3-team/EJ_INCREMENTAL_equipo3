adb5a9b316eb6696c7fedd1a831cc582e5ddb3d9
JoseAntonio <127400010+JoseAntonio50@users.noreply.github.com>
2023-03-27 10:13:15 +0200

    se agregaron funciones extras (#6)

---
+++ javascript/coches.js
@@ -0,0 +1,23 @@
+//EXTRA 14
+
+coches = () => {
+
+    let carros = ['Nissan March', 'Chevrolet Aveo', 'Volkswagen Taos', 'KIA Rio Sedan'];
+    alert(carros);
+
+    carros.pop();
+    alert(carros);
+
+    carros.push('Hyundai Creta');
+    alert(carros);
+
+    carros.shift();
+    alert(carros);
+
+    carros.splice(0,0,'Nissan Versa');
+    alert(carros);
+
+};
+
+// coches()
+
