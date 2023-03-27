62bb26c1b94526eb3b66cff92e3709a8c2ad788f
SilP <pescadorlopezs@gmail.com>
2023-03-27 13:43:57 +0200

    diferenciados eventos pub 1 y pub 2 con fx distintas

--- javascript/likes16.js
+++ javascript/likes16.js
@@ -2,34 +2,42 @@
 let contadorDOM = document.getElementById('sumLikes')
 let contadorDOM2 = document.getElementById('sumLikes2')
 
-//Botones del DOM donde añadimos los eventos click / onclick
+//Botones del DOM donde añadimos los eventos click | para onclick en el propio botón no hace falta
 let likeBtn = document.getElementById('likeBtn')
 let dislikeBtn = document.getElementById('dislikeBtn')
 
 let contadorLikes = 0
 let contadorLikes2 = 0
 
+// EJ. PUBLICACIÓN 1: CREANDO EVENTO CON addEventListenr
 sumLikes = () => {
-      
       contadorLikes = contadorLikes + 1
-      contadorLikes2++
-      contadorDOM.innerHTML = `${contadorLikes}` 
-      contadorDOM2.innerHTML = `${contadorLikes2}` 
-
-}
-
+      contadorDOM.innerHTML = `${contadorLikes}`     
+};
 
 removeLikes = () => {
       contadorLikes = contadorLikes - 1
-      contadorLikes2--
-      contadorDOM.innerHTML = `${contadorLikes}`
+      contadorDOM.innerHTML = `${contadorLikes}` 
+};
+
+likeBtn.addEventListener('click', sumLikes );
+dislikeBtn.addEventListener('click', removeLikes);
+
+// EJ. PUBLICACIÓN 2: CREADO EVENTO DIRECTAMENTE EN EL BOTON CON onclick
+
+//para ello creamos dos funciones nuevas que son llamadas por el onclick.
+sumLikesOn = () =>  {
+      contadorLikes2++
       contadorDOM2.innerHTML = `${contadorLikes2}` 
-      
 }
 
-likeBtn.addEventListener('click', sumLikes )
-dislikeBtn.addEventListener('click', removeLikes)
+removeLikesOn = () => {
+      contadorLikes2--
+      contadorDOM2.innerHTML = `${contadorLikes2}` 
+}
 
+
+// Ejemplo de cambio de HTML en el elemento del DOM que queramos
 
 // changeDom = () => {
 //       if 
