1e33c12b29ed18809e8f4df6a1cf780fd091f6b8
SilP <pescadorlopezs@gmail.com>
2023-03-29 17:33:48 +0200

    incremental 18 integrado

--- javascript/likeDislike.js
+++ javascript/likeDislike.js
@@ -1,10 +1,10 @@
 //Elementos del DOM donde queremos que se renderice el contador actualizado
-let contadorDOM = document.getElementById('sumLikes') // addEventListener
-let contadorDOM2 = document.getElementById('sumLikes2') //onclick
+let contadorDOM = document.getElementById('sumLikes') // CONTADOR PUBLICACION 1 con addEventListener
+let contadorDOM2 = document.getElementById('sumLikes2') //CONTADOR PUBLICACION 2 con onclick
 
 //Botones del DOM donde añadimos los eventos click (para onclick en el propio botón no hace falta)
-let likeBtn = document.getElementById('likeBtn')
-let dislikeBtn = document.getElementById('dislikeBtn')
+let likeBtn = document.getElementById('likeBtn') //BOTÓN PUBLICACION 1 LIKES
+let dislikeBtn = document.getElementById('dislikeBtn')//BOTÓN PUBLICACION 1 DISLIKES
 
 //Inicializamos en cero ambos contadores
 let contadorLikes = 0
@@ -45,57 +45,3 @@ removeLikesOn = () => {
 // changeDom()
 
 
-// INCREMENTAL 18
-
-
-
-let publicacion = {
-      id: 1,
-      author: 'Sandra',
-      likes: 3,
-      avatar: '',
-};
-
-// RESOLUCION CON .THEN .CATCH
-async function getTotalLikes(id) {
-      //esto sería redundante porque al hacer gettotalLikes async ya devuelve una promesa
-      return new Promise((resolve, reject) => {
-            if (publicacion.id === id) {
-                  resolve(publicacion.likes);
-            } else {
-                  reject('La publicación no existe');
-            }
-      });
-}
-
-// Llamada a la función con manejador de la promesa (.then y .catch)
-getTotalLikes(1)
-.then((likes) => {
-      alert(`La publicación tiene ${likes} Likes`);
-})
-.catch((error) => {
-      alert(error);
-});
-
-// RESOLUCION CON ASYNC/AWAIT
-async function getTotalLikesAwait(id) {
-      if (publicacion.id === id) {
-        return publicacion.likes;
-      } else {
-        throw new Error('La publicación no existe');
-      }
-}
-
-// llamada a la función con manejador de la promesa (await y try/catch)
-(async () => {
-      try {
-            const likes = await getTotalLikesAwait(1);
-            alert(`La publicación tiene ${likes} Likes`);
-      } catch (error) {
-            alert(error.message);
-      }
-})();
-
-    
-    
-    
