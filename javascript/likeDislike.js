49e29dbe9c06540416a1af7079423677a3ffa684
SilP <pescadorlopezs@gmail.com>
2023-03-29 12:20:55 +0200

    resolución incremental 18
    hay que cambiarlo a la página de funciones

--- javascript/likeDislike.js
+++ javascript/likeDislike.js
@@ -44,3 +44,58 @@ removeLikesOn = () => {
 
 // changeDom()
 
+
+// INCREMENTAL 18
+
+
+
+let publicacion = {
+      id: 1,
+      author: 'Sandra',
+      likes: 3,
+      avatar: '',
+};
+
+// RESOLUCION CON .THEN .CATCH
+async function getTotalLikes(id) {
+      //esto sería redundante porque al hacer gettotalLikes async ya devuelve una promesa
+      return new Promise((resolve, reject) => {
+            if (publicacion.id === id) {
+                  resolve(publicacion.likes);
+            } else {
+                  reject('La publicación no existe');
+            }
+      });
+}
+
+// Llamada a la función con manejador de la promesa (.then y .catch)
+getTotalLikes(1)
+.then((likes) => {
+      alert(`La publicación tiene ${likes} Likes`);
+})
+.catch((error) => {
+      alert(error);
+});
+
+// RESOLUCION CON ASYNC/AWAIT
+async function getTotalLikesAwait(id) {
+      if (publicacion.id === id) {
+        return publicacion.likes;
+      } else {
+        throw new Error('La publicación no existe');
+      }
+}
+
+// llamada a la función con manejador de la promesa (await y try/catch)
+(async () => {
+      try {
+            const likes = await getTotalLikesAwait(1);
+            alert(`La publicación tiene ${likes} Likes`);
+      } catch (error) {
+            alert(error.message);
+      }
+})();
+
+    
+    
+    
