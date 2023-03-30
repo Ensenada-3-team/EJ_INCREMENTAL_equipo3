5e22b6dad7c2f36ce12a8be18587f77b9b24380d
SilP <pescadorlopezs@gmail.com>
2023-03-30 13:24:57 +0200

    api key en variable de entorno

--- javascript/getPubApi.js
+++ javascript/getPubApi.js
@@ -4,20 +4,15 @@ let fetchBtn = document.getElementById('fetchBtn')
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '01862a3d2fmsh5e05a1ae12a760ep144336jsn8be5c5304cd0',
+		'X-RapidAPI-Key': process.env.API_KEY,
 		'X-RapidAPI-Host': 'instagram-scraper-2022.p.rapidapi.com'
 	}
 };
 
-fetch('https://instagram-scraper-2022.p.rapidapi.com/ig/user_id/?user=spelkit', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
 
 async function getPubApi() {
 
-      await fetch('https://instagram-scraper-2022.p.rapidapi.com/ig/user_id/?user=spelkit', options)
+      await fetch('https://instagram-scraper-2022.p.rapidapi.com/ig/user_id/?user=cr7cristianoronaldo', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
