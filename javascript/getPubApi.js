9da6ee8af0f9f6468f7edf1b94a7167045586b7c
SilP <pescadorlopezs@gmail.com>
2023-03-30 13:30:28 +0200

    comentarios de aclaración

--- javascript/getPubApi.js
+++ javascript/getPubApi.js
@@ -1,6 +1,8 @@
-let fetchBtn = document.getElementById('fetchBtn')
 
-
+//Variable que hay que pasarle a la petición a la api diciendole:
+//1 - El método de la petición http (en este caso es GET porque solicitamos datos)
+//2 - Los encabezados donde se refleja la clave de que estoy registrada y suscrita en la api (en esta concreta se necesita)
+//Nota: he metido la clave en una variable de entorno para que no sea visible para nadie en en el repositorio de gitHub
 const options = {
 	method: 'GET',
 	headers: {
@@ -9,7 +11,7 @@ const options = {
 	}
 };
 
-
+// petición a la api que hemos encapsulado en una función asociada al evento onclic del botón de búsqueda
 async function getPubApi() {
 
       await fetch('https://instagram-scraper-2022.p.rapidapi.com/ig/user_id/?user=cr7cristianoronaldo', options)
