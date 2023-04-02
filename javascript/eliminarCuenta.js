8658c9873a0b3e27228d12dffe95c3aadbc196f2
JoseAntonio <127400010+JoseAntonio50@users.noreply.github.com>
2023-04-02 23:11:45 +0200

    Refactorización Eliminar Cuenta

--- javascript/eliminarCuenta.js
+++ javascript/eliminarCuenta.js
@@ -1,26 +1,12 @@
 // INCREMENTAL 17  - configuracion.html
 
-// Creamos una lista de usuarios y un usuario ficticio
-let usuariosList = [1, 2, 3, "Pepe"];
-
-const usuarioFicticio = {
-	id: 600,
-	nombre: "Jose",
-	apellido: "Gutierrez",
-	email: "equipazo3@example.com",
-};
-
-//accedemos al DOM del botón que elimina cuenta
 const eliminarCuentaBtn = document.querySelector("#eliminar-cuenta");
-// const eliminarcuentaBtnconID = document.getElementById('eliminar-cuenta')
 eliminarCuentaBtn.addEventListener("click", confirmarEliminacionCuenta);
+const msjElim = document.querySelector("#msj-elim");
 
 //RESOLUCIÓN CON ASYNC/AWAIT
 function confirmarEliminacionCuenta() {
-	const confirmacion = confirm(
-		"¿Estás seguro de que deseas eliminar tu cuenta? Esta acción no se puede deshacer."
-	);
-
+	const confirmacion = confirm("¿Estás seguro de que deseas eliminar tu cuenta? Esta acción no se puede deshacer.");
 	if (confirmacion) {
 		eliminarCuenta();
 	}
@@ -28,71 +14,116 @@ function confirmarEliminacionCuenta() {
 
 async function eliminarCuenta() {
 	try {
-		// Simulamos la eliminación de la cuenta del usuario ficticio
-		usuariosList.push(usuarioFicticio);
-
-		console.log(usuariosList);
-		console.table(usuarioFicticio);
-
-		// delete usuarioFicticio;
-		usuariosList.pop();
-
-		console.table(usuarioFicticio);
-		console.log(usuariosList);
-
+		msjElim.innerHTML = "Conectando al servidor... Eliminando Cuenta...";
 		// Esperamos 2 segundos para simular una solicitud asincrónica al servidor
-		await new Promise((resolve) => setTimeout(resolve, 10000));
+		await new Promise((resolve) => setTimeout(resolve, 2000));
 
 		// Redireccionamos al usuario a la página de inicio de sesión
-		// window.location.href = '../login.html';
-		window.open("./login.html", "_blank");
+		window.location.href = "./login.html";
 
 	} catch (error) {
 		console.error(error);
 	}
 }
 
+
+
+// Creamos una lista de usuarios y un usuario ficticio
+//let usuariosList = [1, 2, 3, "Pepe"];
+
+// const usuarioFicticio = {
+// 	id: 600,
+// 	nombre: "Jose",
+// 	apellido: "Gutierrez",
+// 	email: "equipazo3@example.com",
+// };
+
+// usuariosList.push(usuarioFicticio);
+// console.log(usuariosList);
+// console.table(usuarioFicticio);
+
+//accedemos al DOM del botón que elimina cuenta
+//const eliminarCuentaBtn = document.querySelector("#eliminar-cuenta");
+// const eliminarcuentaBtnconID = document.getElementById('eliminar-cuenta')
+//eliminarCuentaBtn.addEventListener("click", confirmarEliminacionCuenta);
+
+//RESOLUCIÓN CON ASYNC/AWAIT
+// function confirmarEliminacionCuenta() {
+// 	const confirmacion = confirm(
+// 		"¿Estás seguro de que deseas eliminar tu cuenta? Esta acción no se puede deshacer."
+// 	);
+
+// 	if (confirmacion) {
+// 		eliminarCuenta();
+// 	}
+// }
+
+// async function eliminarCuenta() {
+// 	try {
+		// Simulamos la eliminación de la cuenta del usuario ficticio
+		//usuariosList.push(usuarioFicticio);
+
+		//console.log(usuariosList);
+		//console.table(usuarioFicticio);
+
+		// delete usuarioFicticio;
+		//usuariosList.pop();
+
+		//console.table(usuarioFicticio);
+		//console.log(usuariosList);
+
+		// Esperamos 2 segundos para simular una solicitud asincrónica al servidor
+		//await new Promise((resolve) => setTimeout(resolve, 10000));
+
+		// Redireccionamos al usuario a la página de inicio de sesión
+		// window.location.href = '../login.html';
+// 		window.open("./login.html", "_blank");
+
+// 	} catch (error) {
+// 		console.error(error);
+// 	}
+// }
+
+
 // RESOLUCIÓN CON FETCH
 
-async function confirmarEliminacionCuentaFetch() {
-	const confirmacion = confirm(
-		"¿Estás seguro de que deseas eliminar tu cuenta? Esta acción no se puede deshacer."
-	);
+// async function confirmarEliminacionCuentaFetch() {
+// 	const confirmacion = confirm(
+// 		"¿Estás seguro de que deseas eliminar tu cuenta? Esta acción no se puede deshacer."
+// 	);
 
-	if (confirmacion) {
-		try {
-			await eliminarCuentaFetch();
+// 	if (confirmacion) {
+// 		try {
+// 			await eliminarCuentaFetch();
 
-		} catch (error) {
-			console.error(error);
-		}
-	}
-}
+// 		} catch (error) {
+// 			console.error(error);
+// 		}
+// 	}
+// }
 
-async function eliminarCuentaFetch() {
+// async function eliminarCuentaFetch() {
 
-	const options = {
-		method: "DELETE",
-		headers: {
-		  Authorization: 
-      		"Bearer " + TOKEN_DE_AUTORIZACION,
-			"Content-Type": "application/json",
-		},
-	};
+// 	const options = {
+// 		method: "DELETE",
+// 		headers: {
+// 			Authorization:
+// 			"Bearer " + TOKEN_DE_AUTORIZACION,
+// 			"Content-Type": "application/json",
+// 		},
+// 	};
 
-  
+// 	const response = await fetch(URL_DEL_ENDPOINT_DE_ELIMINACION_DE_CUENTA, options);
+// 	const data = await response.json();
 
-	const response = await fetch(URL_DEL_ENDPOINT_DE_ELIMINACION_DE_CUENTA, options);
-	const data = await response.json();
-
-	if (response.ok) {
-		redirigirALogin();
-	} else {
+// 	if (response.ok) {
+// 		redirigirALogin();
+// 	} else {
 		// Mostrar mensaje de error al usuario
-		console.error(data.error);
-	}
-}
+// 		console.error(data.error);
+// 	}
+// }
 
-function redirigirALogin() {
-	window.location.href = "./login.html";
-}
+// function redirigirALogin() {
+// 	window.location.href = "./login.html";
+// }
