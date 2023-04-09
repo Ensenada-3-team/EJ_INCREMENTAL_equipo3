30e7a6e6c4d11650022511c21fb6f1a68d6d22c8
SilP <pescadorlopezs@gmail.com>
2023-04-09 12:40:07 +0200

    renombrado y preparado para ej 21

---
+++ silvia/incrementalExtra21.js
@@ -0,0 +1,19 @@
+/*
+ 
+Ejercicio Incremental 21 - OBJETOS
+
+Crear las clases y objetos necesarios para manejar los datos de los usuarios de la red social (perfil).
+Crear un usuario de prueba con nombre: José Hernández.
+Solicitar al menos un atributo por teclado.
+
+Ejercicio Extra 21
+
+Crear una clase llamada “Cuenta” que tendrá los siguientes atributos: titular (que es una persona) y cantidad (puede tener decimales). El titular será obligatorio y la cantidad es opcional. Construye los siguientes métodos para la clase:
+- Un constructor, donde los datos pueden estar vacíos.
+- Los setters y getters para cada uno de los atributos. El atributo no se puede modificar directamente, sólo ingresando o retirando dinero.
+- *mostrar(): Muestra los datos de la cuenta.
+- *ingresar(cantidad): se ingresa una cantidad a la cuenta, si la cantidad introducida es negativa, no se hará nada.
+- *retirar(cantidad): se retira una cantidad a la cuenta. La cuenta puede estar en números rojos.
+
+*/
+
