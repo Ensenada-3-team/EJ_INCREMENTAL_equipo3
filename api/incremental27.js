802fe3780028e2e42406b7b438ad4471c95d257a
SilP <pescadorlopezs@gmail.com>
2023-04-18 11:57:00 +0200

    Create incremental27.js

---
+++ api/incremental27.js
@@ -0,0 +1,17 @@
+const cursos = [
+      'Introducción a Node.js',
+      'Desarrollo web con Express.js',
+      'APIs RESTful con Node.js',
+      'Base de datos SQL'
+]
+
+cursos.forEach(element => console.log(element))
+console.log('__________________________________________')
+console.log('estamos aprendiendo node por las buenas')
+
+
+function decirHola() {
+      console.log('Hola')
+}
+
+decirHola()
