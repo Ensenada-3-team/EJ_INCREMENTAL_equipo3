66a4bd93fba365a2865966807bcb6ed95f17889e
SandraSuarezJimenez <1983ssj@gmail.com>
2023-04-18 19:29:19 +0200

    incremental y extra 27

---
+++ javascript/incrementales/incremental_27.js
@@ -0,0 +1,9 @@
+const cursosRealizados = [
+    "Introducción a JavaScript", 
+    "Desarrollo web con React", 
+    "Programación orientada a objetos en Java", 
+    "Introducción a Python", 
+    "Curso de marketing digital"
+];
+
+cursosRealizados.forEach(curso => console.log(curso));
