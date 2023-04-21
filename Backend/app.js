234e3fffe4232417fc4c05c15cbbd564cd13a559
SilP <pescadorlopezs@gmail.com>
2023-04-21 22:55:29 +0200

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -27,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
 app.use('/auth', authRouter);
+// intereses + cursos...
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
