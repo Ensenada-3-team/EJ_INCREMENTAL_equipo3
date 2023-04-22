51d296b5fb591f174524800aa06cb65dfb5ad715
SilP <pescadorlopezs@gmail.com>
2023-04-22 17:17:04 +0200

    copiados todos los enpoins hechos hasta ahora
    añadidas funciones de extra27 e incremental 28 a carpeta del backend

--- Backend/routes/posts.js
+++ Backend/routes/posts.js
@@ -1,16 +1,21 @@
+const publicaciones = require("../bd-posts");
 var express = require('express');
 var router = express.Router();
 const coolImages = require('cool-images');
 const moment = require('moment');
 
-//Endpoints de los métodos GET y POST
+
 
 /* GET */
+//obtener todas las publicaciones
+router.get("/", async (req, res) => {
 
+})
 
 /* POST */
 
-//peticion fetch desde el frontend a http://localhost:3000/publicaciones
+// Agregar una nueva publicación  
+//peticion fetch desde el frontend a http://localhost:3000/posts/publicaciones
 //(añadir objeto a la peticion con method:POST, headers y body con el contenido pasado a JSON (JSON.stringify) de text y user)
 router.post("/publicaciones", async (req, res) => {
 	const { text, user } = req.body;
