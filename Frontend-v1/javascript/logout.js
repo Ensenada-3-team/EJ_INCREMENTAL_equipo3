3fdd6a2ae74ab34d0e71d67867d0f58a2a0f73b0
SilP <pescadorlopezs@gmail.com>
2023-04-28 14:45:55 +0200

    logout desde zona privada ok

--- Frontend-v1/javascript/logout.js
+++ Frontend-v1/javascript/logout.js
@@ -4,5 +4,5 @@ logoutButton.addEventListener('click', function(event) {
   
   localStorage.removeItem('userData');
  
-  window.location.href = './login.html';
+  window.location.href = './index-login.html';
 });
