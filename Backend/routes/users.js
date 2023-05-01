5455b5dadc468862fd7665f13c333c8648bc2a01
SilP <pescadorlopezs@gmail.com>
2023-05-01 10:55:09 +0200

    endpoint modificar los datos del usuario v1

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -2,15 +2,13 @@ var express = require("express");
 var router = express.Router();
 const pool = require("../db/connection");
 
-
-
 // MIDDLEWARE: COMPRUEBA SI ID ES UN NUMERO
 function isNumber(req, res, next) {
 	// Parseamos el parámetro como un número entero
-	const userId = parseInt(req.params.user_id); 
+	const userId = parseInt(req.params.user_id);
 	if (isNaN(userId)) {
 		// Si no es un número válido devolvemos el error bad request
-		return res.status(400).send("El parámetro id debe ser un número entero"); 
+		return res.status(400).send("El parámetro id debe ser un número entero");
 	}
 	// Si llegamos aquí es porque el parámetro es un número válido, entonces llamamos al siguiente middleware
 	next();
@@ -123,7 +121,41 @@ router.get("/nonfriends/:user_id", async (req, res) => {
 //DELETE- ELIMINAR AMIGO         /:user1_id/friendship/:user2_id
 //UPDATE `friends` SET `status` = '0' WHERE friends.user1_id = '5' AND friends.user2_id = '7';
 
-//DELETE- ELIMINAR USUARIO DE LA BD (CERRAR CUENTA)   
+// PUT - UPDATE USER DATA - 1º busca los datos del usuario en la BD y los actualiza con los datos del body
+router.put("/user/:user_id", async (req, res) => {
+	const userId = req.params.user_id;
+	const userData = req.body;
+
+	try {
+		// Existe el usuario en la bd?
+		const isUser = await pool.query("SELECT * FROM users WHERE user_id = ?", [
+			userId,
+		]);
+
+		// Si no existe, respuesta de error
+		if (isUser.length === 0) {
+			return res.status(404).json({ message: "El usuario no está en la base de datos" });
+		}
+
+		// Copiamos los datos del usuario existente y los mergeamos con los entrantes en el body
+		const updatedUserData = { ...isUser[0], ...userData };
+
+		// Hacemos update de los nuevos datos en la base de datos
+		await pool.query("UPDATE users SET ? WHERE user_id = ?", [
+			updatedUserData,
+			userId,
+		]);
+
+		// Enviamos respuesta al cliente con el resultado
+		res.status(200).json(updatedUserData);
+		
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: "Internal server error" });
+	}
+});
+
+//DELETE- ELIMINAR USUARIO DE LA BD (CERRAR CUENTA)
 router.delete("/delete/:user_id", async (req, res) => {
 	const userId = req.params.user_id;
 	try {
@@ -145,7 +177,5 @@ router.delete("/delete/:user_id", async (req, res) => {
 		res.status(500).json({ error: err.message });
 	}
 });
-
-
 
 module.exports = router;
