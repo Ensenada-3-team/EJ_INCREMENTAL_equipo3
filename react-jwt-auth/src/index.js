a0f1e4f4fef16668f72b872e2606881b47c4c269
SilP <pescadorlopezs@gmail.com>
2023-05-02 12:38:15 +0200

    completado tutorial paper

--- react-jwt-auth/src/index.js
+++ react-jwt-auth/src/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom"
 import './index.css';
+
 import App from './App';
+import * as serviceWorker from "./serviceWorker"
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+ReactDOM.render (
+  <BrowserRouter>
+
     <App />
-  </React.StrictMode>
-);
+  
+  </BrowserRouter>,
+
+  document.getElementById("root")
+)
+
+
+serviceWorker.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
