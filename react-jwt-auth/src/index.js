118dfcbec27ff7bc9af409f5da7f3fdba7db1dd5
SilP <pescadorlopezs@gmail.com>
2023-05-02 13:33:10 +0200

    fix errores de código al compilar

--- react-jwt-auth/src/index.js
+++ react-jwt-auth/src/index.js
@@ -1,33 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom"
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './App';
-import * as serviceWorker from "./serviceWorker"
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const container = document.getElementById("root");
+const root = createRoot(container);
 
-ReactDOM.render (
+root.render(
   <BrowserRouter>
-
     <App />
-  
-  </BrowserRouter>,
-
-  document.getElementById("root")
-)
-
-
-serviceWorker.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  </BrowserRouter>
+);
