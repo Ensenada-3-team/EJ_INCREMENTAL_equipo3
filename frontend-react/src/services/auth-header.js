d303860aa99b221caa1fe4c44e5c5fa65ec640d9
SilP <pescadorlopezs@gmail.com>
2023-05-07 20:05:07 +0200

    Update auth-header.js

--- frontend-react/src/services/auth-header.js
+++ frontend-react/src/services/auth-header.js
@@ -24,13 +24,3 @@ export default function authHeader() {
 
 */
 
-// export default function authHeader() {
-//   const user = JSON.parse(localStorage.getItem('user'));
-
-//   if (user && user.accessToken) {
-//     // for Node.js Express back-end
-//     return { 'x-access-token': user.accessToken };
-//   } else {
-//     return {};
-//   }
-// }
