77f29ff55a7511476a0fe60fdad5972e7bcb49d2
SilP <pescadorlopezs@gmail.com>
2023-05-07 20:54:13 +0200

    correcciones de compilacion

--- frontend-react/src/index.js
+++ frontend-react/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+
 import App from "./App";
 
 const container = document.getElementById("root");
@@ -15,4 +16,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- frontend-react/src/services/auth.service.js
+++ frontend-react/src/services/auth.service.js
@@ -35,4 +35,6 @@ class AuthService {
 	}
 }
 
-export default new AuthService();
+const authService = new AuthService()
+
+export default authService;
