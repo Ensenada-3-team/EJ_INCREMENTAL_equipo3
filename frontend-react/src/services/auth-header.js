d6c41f7f37996ff93b9a2cb949f81a27b093a7dd
SilP <pescadorlopezs@gmail.com>
2023-05-07 19:45:58 +0200

    update del ejemplo

---
+++ frontend-react/src/services/auth-header.js
@@ -0,0 +1,36 @@
+export default function authHeader() {
+	const user = JSON.parse(localStorage.getItem("user"));
+
+	if (user && user.accessToken) {
+		return { Authorization: "Bearer " + user.accessToken };
+	} else {
+		return {};
+	}
+}
+
+/*
+PARA BACKEND CON NODE Y EXPRESS USAR:
+
+export default function authHeader() {
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  if (user && user.accessToken) {
+    // for Node.js Express back-end
+    return { 'x-access-token': user.accessToken };
+  } else {
+    return {};
+  }
+}
+
+*/
+
+// export default function authHeader() {
+//   const user = JSON.parse(localStorage.getItem('user'));
+
+//   if (user && user.accessToken) {
+//     // for Node.js Express back-end
+//     return { 'x-access-token': user.accessToken };
+//   } else {
+//     return {};
+//   }
+// }
