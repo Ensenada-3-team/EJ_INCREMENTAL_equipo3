38cfcf9c9490e641b59c0082f470a3e71d638df7
SilP <pescadorlopezs@gmail.com>
2023-05-09 18:43:29 +0200

    Update auth.service.js

--- frontend-react/src/services/auth.service.js
+++ frontend-react/src/services/auth.service.js
@@ -1,14 +1,14 @@
 import axios from "axios";
+import authHeader from "./auth-header";
 
 const API_URL = "http://localhost:3000/auth/";
 
 class AuthService {
 	async login(nicknameOrEmail, password) {
-		const response = await axios
-			.post(API_URL + "login", {
-				nicknameOrEmail,
-				password,
-			});
+		const response = await axios.post(API_URL + "login", {
+			nicknameOrEmail,
+			password,
+		});
 		if (response.data.token) {
 			localStorage.setItem("user", JSON.stringify(response.data));
 		}
@@ -34,29 +34,54 @@ class AuthService {
 		linkedin,
 		language,
 		hobby
-	    ) {
+	) {
 		try {
-		  const response = await axios.post(API_URL + "register", {
-		    name,
-		    firstname,
-		    nickname,
-		    birthdate,
-		    gender,
-		    avatar,
-		    password,
-		    email,
-		    ocupation,
-		    location,
-		    grade,
-		    linkedin,
-		    language,
-		    hobby,
-		  });
-		  return response;
+			const response = await axios.post(API_URL + "register", {
+				name,
+				firstname,
+				nickname,
+				birthdate,
+				gender,
+				avatar,
+				password,
+				email,
+				ocupation,
+				location,
+				grade,
+				linkedin,
+				language,
+				hobby,
+			});
+			return response;
 		} catch (error) {
-		  console.log(error);
+			console.log(error);
+		}
+	}
+
+	async changePassword(userId, oldPassword, password) {
+		try {
+		  const response = await axios.put(
+		    "/change-password",
+		    {
+			userId,
+			oldPassword,
+			password,
+		    }, 
+
+		    { headers: authHeader() }
+		  );
+	    
+		  return response.data;
+		} catch (error) {
+		  if (error.response) {
+		    throw new Error(error.response.data.message);
+		  } else {
+		    throw error;
+		  }
 		}
 	    }
+
+
 
 	getCurrentUser() {
 		return JSON.parse(localStorage.getItem("user"));
