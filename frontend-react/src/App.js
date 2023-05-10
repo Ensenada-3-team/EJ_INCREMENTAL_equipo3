9b9d9a07d4b09d5ad114835750a49d6414aeab27
SilP <pescadorlopezs@gmail.com>
2023-05-10 14:22:09 +0200

    instalado en app.js las funcionalidades js de bootstrap
    import React ,{ useEffect } from "react";
    import $ from 'jquery';
    import Popper from 'popper.js';
    import 'bootstrap/dist/css/bootstrap.min.css';
    import 'bootstrap/dist/js/bootstrap.bundle.min.js';
    
    ahora el toggle bar funciona

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React ,{ useEffect } from "react";
 // import { BrowserRouter as Router, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import "./App.css";
 
 import Navbar from "./components/Navbar";
